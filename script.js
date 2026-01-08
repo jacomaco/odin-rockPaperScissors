2eeeffd853601317434b2c2f48ed3d67943fa4a7
Jacob Johansson <jacobknuts@gmail.com>
2026-01-08 15:49:12 +0100

    added getHumanChoice function and selectHand helper function
    selectHand was added to reduce code redundancy

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+console.log(getComputerChoice());
+console.log(getHumanChoice())
+
 function getComputerChoice() {
-    let numberOfHands = 3;
-    switch (Math.floor((Math.random() * numberOfHands) + 1)) {
-        case 1:
-            return "rock";
-            break;
-        case 2:
-            return "paper";
-            break;
-        case 3:
-            return "scissors";
-            break;
-        default:
-            return "invalid hand";
-            break;
-    }
+  let numberOfHands = 3;
+  let hand = Math.floor(Math.random() * numberOfHands + 1);
+  return selectHand(hand);
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    return selectHand(parseInt(prompt("Choose your hand; rock: 1, paper: 2, scissor: 3", 1)));
+}
+
+function selectHand(hand) {
+  switch (hand) {
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissors";
+      break;
+    default:
+      return "rock";
+      break;
+  }
+}
